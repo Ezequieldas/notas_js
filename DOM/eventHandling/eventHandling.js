ec81e42bbc54f3a5159356ed3f782612a2966b40
Ezequiel Dastolfo <ezequieldastolfo@gmail.com>
2022-11-22 19:46:07 -0500

    events

--- DOM/eventHandling/eventHandling.js
+++ DOM/eventHandling/eventHandling.js
@@ -3,6 +3,9 @@ function helloWorld() {
   console.log(event)
 }
 
-const $eventoSemantico = document.getElementById("evento-semantico");
+const $eventoSemantico = document.getElementById("evento-semantico"),
+$eventoMultiple = document.getElementById("evento-multiple")
 
 $eventoSemantico.onclick = helloWorld;
+
+$eventoMultiple.addEventListener("click", helloWorld)
